9358b87925e855e76b523677b269e84867d29e02
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2013-06-22 03:22:58 +0200

    22/06/2013 03:22
    Modifiche al cloud

--- php/www.lewe.tk/views/pages/presentation/site_content_how-it-works/how-it-works.php
+++ php/www.lewe.tk/views/pages/presentation/site_content_how-it-works/how-it-works.php
@@ -16,37 +16,46 @@
 	<div class="container">
     
         <div id="how-it-works_container">
-        	<div class="paragraph_home">
-                <img src="/images/park.jpg" class="img_home_sx" id="img_home1" />
-                <p class="paragraph1">Alessandro è un patito del brivido, ama smisuratamente l’arrampicata e il parkour.
-                La sua settimana è impegnativa e stressante e solo il venerdì può finalmente dedicarsi ai suoi sport preferiti.
-                Fare questi sport significa misurarsi con i propri limiti, provare forti emozioni e solo un duro allenamento gli garantisce di raggiungere i suoi obbiettivi: originalità e spettacolarità del movimento.
-                Andrea, il personal trainer di Alessandro, sa che la preparazione atletica del suo amico richiede un continuo controllo, è necessario monitorare costantemente i sui parametri vitali.
-                Seguirlo nelle sue avventure è impossibile ma la sua presenza è molto importante per registrare in modo costante le possibilità fisiche del momento.</p>
+        	<h2 id="title-how-it-works">Come funziona Lewe?</h2>
+        	<div class="paragraph_how-it-works">
+                <img src="/images/park.jpg" class="img_how-it-works_sx" id="img_how-it-works1" />
+                <p class="paragraph1">Alessandro è un patito del brivido, ama smisuratamente l’arrampicata e il parkour.</p>
+                <p class="paragraph1">La sua settimana è impegnativa e stressante e solo il venerdì può finalmente dedicarsi ai suoi sport preferiti.</p>
+                <p class="paragraph1">Praticarli significa misurarsi con i propri limiti, provare forti emozioni, ma solo un duro allenamento gli garantisce di raggiungere i suoi obiettivi: originalità e spettacolarità del movimento.</p>
+                <p class="paragraph1">Andrea, il suo personal trainer, sa che la preparazione atletica richiede un continuo controllo e un costante monitoraggio dei suoi parametri vitali.</p>
+				<p class="paragraph1">Seguire Alessandro nelle sue avventure sarebbe impossibile, ma l'assistenza di Andrea è indispensabile per registrare le possibilità fisiche del momento.</p>
+</p>
             </div>
             
-            <div class="paragraph_home">
-                <img src="/images/brac.png" class="img_home_dx" id="img_home2" />
-                <p class="paragraph2">Il bracciale Lewe è una valida idea per aiutare i nostri due amici. </p>
+            <div class="paragraph_how-it-works">
+                <img src="/images/brac.png" class="img_how-it-works_dx" id="img_how-it-works2" />
+                <p class="paragraph2">Il bracciale Lewe è la soluzione perfetta! </p>
             </div>
             
-            <div class="paragraph_home">
-            	<p class="paragraph3">Andrea può conoscere in tempo reale alcuni dati biometrici di Alessandro semplicemente accedendo al cloud online di Lewe.</p>
+            <div class="paragraph_how-it-works">
+            	<p class="paragraph3">Andrea può conoscere in tempo reale i dati biometrici di Alessandro semplicemente accedendo al cloud online di Lewe e seguirlo nella sua preparazione atletica ovunque si trovi.</p>
             </div>
             
-        	<div class="paragraph_home">
-                <img src="/images/cloud.png" class="img_home_sx" id="img_home3" />
-                <p class="paragraph4">Lewe non è solo un bracciale biometrico che offre la possibilità di rilevare la temperatura e la sudorazione del corpo e di inviare i valori ad un applicazione installata in uno Smartphone ma permette anche di caricare i dati in un cloud online gratuito dove potranno essere  accessibili in qualunque parte del mondo.</p>
+        	<div class="paragraph_how-it-works">
+                <img src="/images/cloud.png" class="img_how-it-works_sx" id="img_how-it-works3" />
+                <p class="paragraph4">Perché Lewe non è un banale bracciale biometrico! Con Lewe è possibile:</p>
+                <ol id="paragraph4-list">
+                    <li class="paragraph4-list-item">Rilevare la temperatura e la sudorazione del corpo <li>
+                    <li class="paragraph4-list-item">Inviare i valori ad un applicazione installata su uno Smartphone<li>
+                    <li class="paragraph4-list-item">Caricare gratuitamente online i dati su un cloud<li>
+                    <li class="paragraph4-list-item">Accedere ai dati da qualunque parte del mondo<li>
+                    <li class="paragraph4-list-item">Organizzare i dati in semplici grafici che consentono di conoscere l’andamento nel tempo dei parametri vitali e di confrontarli con quelli di altri utilizzatori.<li>
+                </ol>
         	</div>
             
-            <div class="paragraph_home">
-                <img src="/images/tel.png" class="img_home_dx" id="img_home4" />
+            <div class="paragraph_how-it-works">
+                <img src="/images/tel.png" class="img_how-it-works_dx" id="img_how-it-works4" />
                 <p class="paragraph5">Il meccanismo è semplice: Alessandro acquista il bracciale e scarica gratuitamente dal suo store l’applicazione e registra un account nel cloud di Lewe; dopo aver configurato adeguatamente l’app con i suoi dati di accesso al portale, autorizza Andrea a consultare i suoi dati biometrici.
              	In questo modo Andrea, semplicemente entrando nel suo account nel cloud, può monitorare alcuni parametri vitali di Alessandro in tempo reale.</p>
         	</div>
             
-            <div class="paragraph_home">
-                <img src="/images/tel2.png" class="img_home_sx" id="img_home5" />
+            <div class="paragraph_how-it-works">
+                <img src="/images/tel2.png" class="img_how-it-works_sx" id="img_how-it-works5" />
                 <p class="paragraph6">Lewe organizza anche i dati in semplici grafici che consentono di conoscere l’andamento nel tempo dei parametri vitali del suo utilizzatore e di agevolare i confronti tra due o più utilizzatori.</p>
 			</div>
         </div>
